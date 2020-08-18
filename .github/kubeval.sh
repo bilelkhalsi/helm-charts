57ca36bd960d22f7e3ff174a9711822160292efd
iozone <bikhalsi@gmail.com>
2020-08-18 03:23:21 +0200

    kubeval-chart: config fix

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -2,8 +2,7 @@
 set -euo pipefail
 GIT_REVISION="$(git rev-parse --abbrev-ref HEAD)"
 echo $GIT_REVISION
-CHART_DIRS="$(git diff --find-renames --name-only $GIT_REVISION master -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
-echo $CHART_DIRS
+CHART_DIRS="$(git diff --find-renames --name-only $GIT_REVISION remotes/origin/master -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
 KUBEVAL_VERSION="0.14.0"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 
