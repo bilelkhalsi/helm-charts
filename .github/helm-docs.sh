ebf9e6e6e1d5a45366c7586c588caf7ec276cafd
bilelkhalsi <bilel_khalsi@live.fr>
2020-11-08 00:00:27 +0100

    refactor: github workflows refactored

--- .github/helm-docs.sh
+++ .github/helm-docs.sh
@@ -8,5 +8,5 @@ curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz http
 tar -xf /tmp/helm-docs.tar.gz helm-docs
 
 # validate docs
-./helm-docs $1
-git diff --exit-code $1
+./helm-docs
+git diff --exit-code charts 
--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -euo pipefail
-echo "Kubeval : validate charts on k8s : ${KUBERNETES_VERSION#v}" 
-GIT_REVISION="$(git rev-parse --abbrev-ref HEAD)"
-CHART_DIRS="$(git diff --find-renames --name-only $GIT_REVISION remotes/origin/master -- charts | grep '[cC]hart.yaml' | sed -e 's/[Cc]hart.yaml//g')"
-echo "Charts to validate : ${CHART_DIRS}" 
+echo "Kubeval : validate ${CHART_DIR} Helm Chart on k8s : ${KUBERNETES_VERSION#v}" 
+
 KUBEVAL_VERSION="0.14.0"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 
@@ -11,7 +9,5 @@ SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-s
 curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
 tar -xf /tmp/kubeval.tar.gz kubeval
 
-# validate charts
-for CHART_DIR in ${CHART_DIRS}; do
-  helm template "${CHART_DIR}" | ./kubeval --strict --ignore-missing-schemas --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}"
-done
+# validate chart
+helm template "${CHART_DIR}" | ./kubeval --strict --ignore-missing-schemas --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}"
