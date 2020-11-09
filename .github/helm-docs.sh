e31ca3e9154a2980a08e0295dee0869c4ea5aa3f
bilelkhalsi <bilel_khalsi@live.fr>
2020-11-09 02:28:48 +0100

    istio-virtual-service: docs lint fixed

--- .github/helm-docs.sh
+++ .github/helm-docs.sh
@@ -6,7 +6,8 @@ HELM_DOCS_VERSION="1.4.0"
 # install helm-docs
 curl --silent --show-error --fail --location --output /tmp/helm-docs.tar.gz https://github.com/norwoodj/helm-docs/releases/download/v"${HELM_DOCS_VERSION}"/helm-docs_"${HELM_DOCS_VERSION}"_Linux_x86_64.tar.gz
 tar -xf /tmp/helm-docs.tar.gz helm-docs
-
+# generate HELM docs
+./helm-docs --dry-run
 # validate docs
 ./helm-docs
 git diff --exit-code charts 
